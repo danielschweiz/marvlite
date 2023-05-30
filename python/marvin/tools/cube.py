e17f3fe47d3878fe05dd8bde48893a9612b05c33
danielschweiz <135059715+danielschweiz@users.noreply.github.com>
2023-05-30 12:32:11 -0700

    Update cube.py

--- python/marvin/tools/cube.py
+++ python/marvin/tools/cube.py
@@ -3,3 +3,12 @@ frim astropy.io import fits
 from astropy.wcs import WCS
 
 import marvin
+
+class Cube():
+  
+  def __init__(self, input=None, filename=None, mangaid=None, plateifu=None,
+               mode=None, data=None, release=None, 
+               drpall=None, download=None, nsa_source='auto')
+    self.header = None
+    self.
+
