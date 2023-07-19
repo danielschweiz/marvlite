99dc384df684b2389f44456abd72ac231579dd93
benjamin herrera <135060520+bekn33@users.noreply.github.com>
2023-07-19 19:40:06 +0000

    stuff

--- TEST/python/cube.py
+++ TEST/python/cube.py
@@ -1,15 +1,13 @@
 import os
 
 class DataCube:
-    # cube properties:
-        # get_filename: takes file's name from given file path
+    '''Class that can pull all necessary functional information from an inputted datacube.'''
 
-    def __init__ (self, file_name):
-    
+    def __init__ (self, file_name):   
         file_name = None
 
     def get_filename(self, fullpath):
-        
-        filename = os.path.basename(fullpath)
-        return filename
+         '''Gets filename from a given file path. Planning on adding a check to confirm whether or not the file is a .fits.gz file.'''      
+         filename = os.path.basename(fullpath)
+         return filename
     
